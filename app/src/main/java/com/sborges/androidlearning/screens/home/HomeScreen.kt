195fa046161dd85a11b9f8c62c15ead9e5a29129
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-12-05 16:27:55 -0300

    Enhance HomeScreen with background color and add preview function

--- app/src/main/java/com/sborges/androidlearning/screens/home/HomeScreen.kt
+++ app/src/main/java/com/sborges/androidlearning/screens/home/HomeScreen.kt
@@ -1,18 +1,31 @@
 package com.sborges.androidlearning.screens.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier
+            .background(Color(0xFF4CAF50))
+            .fillMaxSize()
     ) {
 
     }
 
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    Column {
+        HomeScreen()
+    }
 }
