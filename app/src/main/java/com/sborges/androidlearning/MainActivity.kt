91e8d83be0bdcd4bab9711ecf8a3427f31856da8
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-12-05 16:17:52 -0300

    Initialize Android project with Gradle setup and basic UI components

--- app/src/main/java/com/sborges/androidlearning/MainActivity.kt
+++ app/src/main/java/com/sborges/androidlearning/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.sborges.androidlearning.screens.home.HomeScreen
 import com.sborges.androidlearning.ui.theme.AndroidLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +18,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidLearningTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidLearningTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/sborges/androidlearning/screens/home/HomeScreen.kt
@@ -0,0 +1,18 @@
+package com.sborges.androidlearning.screens.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
+
+}
